085e8b86a87bc1a9cb7e93b9e2e44a79a28092e4
Matthew Vallance <matthew.vallance@outlook.com>
2025-08-19 16:56:06 -0400

    Type inference continued

--- lln-course/code_notes/type_inference.cpp
+++ lln-course/code_notes/type_inference.cpp
@@ -7,23 +7,23 @@
 #include <iostream>
 
 int main() {
-    auto score;
-    auto points;
-    auto height;
-    auto duration;
-    auto is_active;
-    auto initial;
-    auto title;
+    auto score = 9; // Int
+    auto points = 19314858291984221; // Int over 32-bit
+    auto height = 6.3f; // Float (f = constant specifier for floats)
+    auto duration = 90.0; // Float (no trailing f - double by default)
+    auto is_active = true; // Boolean
+    auto initial = 'P'; // Char
+    auto title = "Soccar Champions"; // String
 
 
 
     std::cout << "Score type: " << typeid(score).name() << std::endl;
-    std::cout << " type: " << typeid(points).name() << std::endl;
-    std::cout << " type: " << typeid(height).name() << std::endl;
-    std::cout << " type: " << typeid(duration).name() << std::endl;
-    std::cout << " type: " << typeid(is_active).name() << std::endl;
-    std::cout << " type: " << typeid(initial).name() << std::endl;
-    std::cout << " type: " << typeid(title).name() << std::endl;
+    std::cout << "Points type: " << typeid(points).name() << std::endl;
+    std::cout << "Height type: " << typeid(height).name() << std::endl;
+    std::cout << "Duration type: " << typeid(duration).name() << std::endl;
+    std::cout << "Is active type: " << typeid(is_active).name() << std::endl;
+    std::cout << "Initial type: " << typeid(initial).name() << std::endl;
+    std::cout << "Title type: " << typeid(title).name() << std::endl;
 
     return 0;
 }
