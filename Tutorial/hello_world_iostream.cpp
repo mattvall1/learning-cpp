d6d0199cc7d38e514915cc31f459509774682f87
Matthew Vallance <matthew.vallance@outlook.com>
2025-08-19 11:31:17 -0400

    For running from terminal hello world

---
+++ Tutorial/hello_world_iostream.cpp
@@ -0,0 +1,13 @@
+/*
+Third program
+*/
+
+#include <iostream>
+/* Takes two args
+argc = int: represents number of command line arguments
+argv = array: C-style strings representing ^ arguments
+*/
+int main(int argc, char* argv[]) {
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
