cb17c4537be8c669f0f361ddfd5845f78499eda7
Matthew Vallance <matthew.vallance@outlook.com>
2025-08-19 11:40:53 -0400

    Add numbers + CLion settings

---
+++ Tutorial/add_numbers.cpp
@@ -0,0 +1,15 @@
+/*
+ Program to add two numbers together
+*/
+
+#include <iostream>
+
+int add(int a, int b) {
+    return a + b;
+
+}
+
+int main() {
+    int result = add(5, 6);
+    printf("Result: %d \n", result);
+}
