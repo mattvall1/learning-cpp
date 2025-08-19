e89a3e53906d9531d0ecfa78aba12dc99bb326bb
Matthew Vallance <matthew.vallance@outlook.com>
2025-08-19 11:14:18 -0400

    hello_world program

---
+++ Tutorial/hello_world.cpp
@@ -0,0 +1,13 @@
+//
+// First program
+//
+
+// Note: iostream (standard library), print text to screen, recieve from keyboard etc
+#include <iostream>
+
+// Main function must return int
+int main() {
+    // :: = Scope resolution operator | cout = Character Output | << = Leftbound insertion operator | endl = End line
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
