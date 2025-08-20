14c78be1086d207bbef0fc545eef3b9764a3d4e3
Matthew Vallance <matthew.vallance@outlook.com>
2025-08-20 10:53:38 -0400

    Add solution + notes

--- lln-course/code_challenges/c2_calculating_averages.cpp
+++ lln-course/code_challenges/c2_calculating_averages.cpp
@@ -6,7 +6,15 @@ double my_average(int a, int8_t b, uint32_t c){
     double result = 0.0;
 
     result = a + b + static_cast<int32_t>(c);
-    result = result / 3;
+    result = result / 3; // '3' completes integer division, so works separately after conversion. But the provided solution is better
+
+    return result;
+}
+
+double my_average_solution(int a, int8_t b, uint32_t c){
+    double result = 0.0;
+
+    result = (a + b + c) / 3.0; // 3.0 returns double here - better solution than mine
 
     return result;
 }
@@ -19,6 +27,10 @@ int main() {
     double result = my_average(a, b, c);
 
     printf("Result: %f \n", result);
+
+    double result_s = my_average(a, b, c);
+
+    printf("Solution result: %f \n", result_s);
 
     return 0;
 }
