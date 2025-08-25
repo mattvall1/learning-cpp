32b4326d896bd9abf6750f6dec0665699897e933
Matthew Vallance <matthew.vallance@outlook.com>
2025-08-25 11:55:37 -0400

    Reading additional functionality

--- lln-course/code_notes/file_handling_reading.cpp
+++ lln-course/code_notes/file_handling_reading.cpp
@@ -7,20 +7,43 @@
 #include <fstream>
 
 int main() {
-    std::ifstream in_file;
+    std::ifstream desc_file;
+    std::ifstream data_file;
     std::string str;
-    int number;
-    char letter;
+    // Could be moved to inner scope, but keep here for readability
+    std::string name;
+    int age;
+    char initial;
 
-    in_file.open("file_handling.txt");
-    if (in_file.fail()) {
+    // Simply reading a file and printing line-by-line
+    desc_file.open("file_handling.txt");
+    if (desc_file.fail()) {
         std::cout << std::endl << "File not found." << std::endl;
     } else {
-        while (!in_file.eof()) { // While not end of file - this iterates each line
-            getline(in_file, str);
+        while (!desc_file.eof()) { // While not end of file - this iterates each line
+            getline(desc_file, str);
             std::cout << str << std::endl;
         }
-        in_file.close();
+        desc_file.close();
+    }
+
+    // Processing the data
+    data_file.open("file_handling_data.txt");
+    if (data_file.fail()) {
+        std::cout << std::endl << "File not found." << std::endl;
+    } else {
+        std::cout << "Name, Age, Initial" << std::endl;
+        while (!data_file.eof()) { // While not end of file - this iterates each line
+            getline(data_file, name);
+            std::cout << name << ", ";
+            getline(data_file, str);
+            age = stoi(str);
+            std::cout << age << ", ";
+            getline(data_file, str);
+            initial = str[0];
+            std::cout << initial << std::endl;
+        }
+        data_file.close();
     }
 
     return 0;
